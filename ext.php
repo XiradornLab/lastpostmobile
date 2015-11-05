68da2e9321059a0aad527e017fd16177e73c48ca
Xiradorn <xiradorn@gmail.com>
2015-11-05 22:49:37 +0100

    First Commit

---
+++ ext.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package Last Post non Mobile View
+* @copyright (c) 2015 Sir Xiradorn
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace xiradorn\lastpostmobile;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
